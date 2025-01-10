907a76d587d31dcb43bdd69b47b2810a699f3b7a
BeneficialCode <3123132899@qq.com>
2025-01-10 19:29:59 +0800

    添加项目文件。

---
+++ KillFileOwner/KillFileOwner.cpp
@@ -0,0 +1,65 @@
+﻿// KillFileOwner.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <Windows.h>
+#include <RestartManager.h>
+#include <stdio.h>
+
+#pragma comment(lib,"Rstrtmgr.lib")
+
+BOOL GetFileOwner(LPCWSTR pathName) {
+    WCHAR sessionKey[CCH_RM_SESSION_KEY + 1] = { 0 };
+    DWORD hSession = 0;
+    if (RmStartSession(&hSession, 0, sessionKey) == ERROR_SUCCESS) {
+        if (RmRegisterResources(hSession, 1, &pathName, 0, nullptr, 0, nullptr) == ERROR_SUCCESS) {
+            UINT nProcInfoNeeded = 0;
+            UINT nProcInfo = 0;
+            DWORD reason = 0;
+
+            do
+            {
+                DWORD ret = RmGetList(hSession, &nProcInfoNeeded, &nProcInfo, nullptr, &reason);
+
+                if (ret != ERROR_MORE_DATA || !nProcInfoNeeded) {
+                    RmEndSession(hSession);
+                    return FALSE;
+                }
+
+                PRM_PROCESS_INFO pRPI = (PRM_PROCESS_INFO)malloc(sizeof(RM_PROCESS_INFO) * nProcInfoNeeded);
+                if (!pRPI) {
+                    RmEndSession(hSession);
+                    return FALSE;
+                }
+
+                nProcInfo = nProcInfoNeeded;
+                ret = RmGetList(hSession, &nProcInfoNeeded, &nProcInfo, pRPI, &reason);
+                if (ret == ERROR_MORE_DATA) {
+                    continue;
+                }
+                if (ret != ERROR_SUCCESS || !nProcInfoNeeded) {
+                    free(pRPI);
+                    RmEndSession(hSession);
+                }
+
+                for (int i = 0; i < nProcInfo; i++) {
+                    printf("process id: %d, process name: %ws\n", pRPI[i].Process.dwProcessId, pRPI[i].strAppName);
+                }
+                RmShutdown(hSession, RmForceShutdown, nullptr);
+            } while (FALSE);
+            
+            
+        }
+
+        RmEndSession(hSession);
+        return TRUE;
+    }
+    DWORD error = ::GetLastError();
+    printf("error: %d\n", error);
+    return FALSE;
+}
+
+int main(){
+    WCHAR file[] = L"D:\\Dev\\cpp\\KillFileOwner\\Debug\\hello.txt";
+    GetFileOwner(file);
+    return 0;
+}
+
---
+++ LockFile/LockFile.cpp
@@ -0,0 +1,24 @@
+﻿// LockFile.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <Windows.h>
+#include <stdio.h>
+
+int wmain(int argc,wchar_t* argv[])
+{
+	HANDLE hFile = ::CreateFile(argv[1], GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, nullptr);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		return 1;
+	}
+
+	do
+	{
+		printf("Hey! I held the %ws file\n", argv[1]);
+		Sleep(5000);
+	} while (true);
+
+	::CloseHandle(hFile);
+
+	return 0;
+}
+
+
